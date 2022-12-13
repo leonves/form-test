638a962c83b48c00db30e13c48d38ad489e6a50c
Leonardo Neves <leonevesaraujo@outlook.com>
2022-12-12 21:15:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
